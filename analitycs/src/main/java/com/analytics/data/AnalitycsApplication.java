1e4236e4216c99b5c46c5c0a989c6147761aced6
LuizLiberatoscioli <92382006+LuizLiberatoscioli@users.noreply.github.com>
2023-11-12 12:57:41 -0500

    start analitycs

---
+++ analitycs/src/main/java/com/analytics/data/AnalitycsApplication.java
@@ -0,0 +1,13 @@
+package com.analytics.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalitycsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalitycsApplication.class, args);
+	}
+
+}
